a47e6d15d11ccc56c0f74363b3fd50e0e26a4577
Nyong-jeanmarc <Nyongjean33@gmail.com>
2025-03-16 19:16:20 +0100

    modified listening port

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.use(express.json());
 
 app.post('/api/sessions', createSessionController);
 
+
 const port = 3000;
 app.listen(port, async () => {
   console.log(`Server listening on port ${port}`);
